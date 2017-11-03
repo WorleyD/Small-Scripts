48b3e40b3caed46f877109c7adbaa6471874c0a4
David Worley <worleyd@uwindsor.ca>
2017-11-03 12:56:15 -0400

    Update KaBoom!.py
    Added Comments, question to be added later

--- KaBoom!.py
+++ KaBoom!.py
@@ -1,20 +1,24 @@
+#Problem was located on ACM ECNA Regional Practice Problems, question will be provided once available.
+#Problem mostly tied into high school physics/math. Some trig and dynamics equations were used.
+#Comments will be updated to better reflect question once available
 
 import math
 
 def main():
-    xm, vx, vy, xa, va, tk = (map(int, input().split()))
+    xm, vx, vy, xa, va, tk = (map(int, input().split())) #get all necessary input
 
-    xd = float(xm + vx*tk - xa)
+    xd = float(xm + vx*tk - xa)      #calculate x and y distance
     yd = float(vy*tk - 16*tk*tk)
 
-    hypd = (xd*xd + yd*yd)**0.5
+    hypd = (xd*xd + yd*yd)**0.5       #calculate hypoteneuse
 
-    t = hypd/va
-    a = math.degrees(math.asin(yd/hypd))
-
+    t = hypd/va                       #calculate time and angle  
+    a = math.degrees(math.asin(yd/hypd)) 
+    
+    #if we can not destroy the missile (would have needed to fire before it launched, or their missile has already landed)
     if (t > tk or yd <= 0):
-        print("start running")
+        print("start running")      #sound alarm
     else:
-        print(tk-t, a)
+        print(tk-t, a)              #otherwise print when to shoot our missile, and the angle to shoot it at
 
 main()
